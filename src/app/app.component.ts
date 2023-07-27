7f2a3116d6d32513bcbbec092fb8252252b4eb55
shantanu <iamshantanudas17@gmail.com>
2023-07-27 19:10:33 +0530

    added navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'e-commerce-frontend';
+
+  searchItem: string = "";
+  cartCount: number = 0;
+
+  onSubmit() {
+    console.log(this.searchItem);
+    this.cartCount += 1;
+  }
 }
