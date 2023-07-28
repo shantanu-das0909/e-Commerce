399db4c751a2c7a84bb0500a928b3068e2a14af2
shantanu <iamshantanudas17@gmail.com>
2023-07-28 10:50:48 +0530

    added home-category section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './home/home.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
