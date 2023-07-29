51acfa67438044ef9cc34f2849997de6079658d8
shantanu <iamshantanudas17@gmail.com>
2023-07-29 15:33:58 +0530

    updated right side discount part in discount-category section in home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,29 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { HomeService } from '../services/home.service';
 
-interface discountImages{
+interface firstShoppingDiscountsData {
   imageSrc: string;
   imageAlt: string;
+  productType: string;
+  discountHeader: string;
+  discountDesc: string;
 }
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-
-  images: discountImages[] = [];
+  firstShoppingDiscountsData: firstShoppingDiscountsData[] = [];
   selectedImageIndex: number = 0;
   slideInterval = 3000;
   autoSlider: boolean = true;
 
-  constructor(private homeService: HomeService){
-
-  }
+  constructor(private homeService: HomeService) {}
   ngOnInit(): void {
-    this.images = this.homeService.getDeiscountCarouselData();
-    if(this.autoSlider) {
+    this.firstShoppingDiscountsData = this.homeService.getDeiscountCarouselData();
+    if (this.autoSlider) {
       this.autoSlideImages();
     }
   }
@@ -42,21 +42,28 @@ export class HomeComponent implements OnInit {
 
   //move to prev image
   onPrevClick(): void {
-    if(this.selectedImageIndex === 0) {
-      this.selectedImageIndex = this.images.length-1;
-    }
-    else {
+    if (this.selectedImageIndex === 0) {
+      this.selectedImageIndex = this.firstShoppingDiscountsData.length - 1;
+    } else {
       this.selectedImageIndex--;
     }
   }
 
   //move to next image
   onNextClick(): void {
-    if(this.selectedImageIndex === this.images.length-1) {
+    if (this.selectedImageIndex === this.firstShoppingDiscountsData.length - 1) {
       this.selectedImageIndex = 0;
-    }
-    else {
+    } else {
       this.selectedImageIndex++;
     }
   }
+
+
+  // @ViewChild('testRefernce', {static: true}) testRefernce = {} as ElementRef;
+  // test() {
+  //   // console.log(this.testRefernce.nativeElement);
+  //   let childElm = this.testRefernce.nativeElement.children[0];
+  //   let items = childElm.getElementsByClassName("test-item");
+  //   childElm.append(items[0]);
+  // }
 }
--- src/app/services/home.service.ts
+++ src/app/services/home.service.ts
@@ -1,30 +1,71 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HomeService {
+  constructor() {}
 
-  constructor() { }
-
-  // discount section data 
-  images = [
+  // discount section data
+  firstShoppingDiscountsData = [
     {
-      imageSrc: "https://images.unsplash.com/photo-1469474968028-56623f02e42e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bmF0dXJlfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
-      imageAlt: "nature1"
+      imageSrc:
+        'https://images.unsplash.com/photo-1546868871-7041f2a55e12?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8c21hcnQlMjB3YXRjaHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60',
+      imageAlt: 'watch',
+      productType: 'watch',
+      discountHeader: '20% Off For Your First Shopping',
+      discountDesc:
+        'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque laudantium illum facere harum cumque dolor laboriosam, atque amet iste expedita!',
     },
     {
-      imageSrc: "https://images.unsplash.com/photo-1472214103451-9374bd1c798e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bmF0dXJlfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
-      imageAlt: "nature2",
+      imageSrc:
+        'https://cdn.pixabay.com/photo/2013/07/12/18/39/smartphone-153650_640.png',
+      imageAlt: 'mobile',
+      productType: 'mobile',
+      discountHeader: '20% Off For Your First Shopping',
+      discountDesc:
+        'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque laudantium illum facere harum cumque dolor laboriosam, atque amet iste expedita!',
     },
     {
-      imageSrc: "https://images.unsplash.com/photo-1501854140801-50d01698950b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8bmF0dXJlfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
-      imageAlt: "nature3"
-    }
+      imageSrc:
+        'https://cdn.pixabay.com/photo/2015/08/10/20/17/handbag-883122_640.jpg',
+      imageAlt: 'bag',
+      productType: 'bag',
+      discountHeader: '20% Off For Your First Shopping',
+      discountDesc:
+        'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque laudantium illum facere harum cumque dolor laboriosam, atque amet iste expedita!',
+    },
+    {
+      imageSrc:
+        'https://cdn.pixabay.com/photo/2013/07/13/12/17/headphone-159569_640.png',
+      imageAlt: 'headphone',
+      productType: 'headphone',
+      discountHeader: '20% Off For Your First Shopping',
+      discountDesc:
+        'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque laudantium illum facere harum cumque dolor laboriosam, atque amet iste expedita!',
+    },
+    {
+      imageSrc:
+        'https://images.unsplash.com/photo-1545289414-1c3cb1c06238?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTB8fHNob2V8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60',
+      imageAlt: 'shoe',
+      productType: 'shoe',
+      discountHeader: '20% Off For Your First Shopping',
+      discountDesc:
+        'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque laudantium illum facere harum cumque dolor laboriosam, atque amet iste expedita!',
+    },
+    {
+      imageSrc:
+        'https://cdn.pixabay.com/photo/2017/11/04/22/14/gift-2919003_640.png',
+      imageAlt: 'gift',
+      productType: 'gift',
+      discountHeader: '20% Off For Your First Shopping',
+      discountDesc:
+        'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque laudantium illum facere harum cumque dolor laboriosam, atque amet iste expedita!',
+    },
   ];
 
   //returning discount section data
   getDeiscountCarouselData() {
-    return this.images;
+    return this.firstShoppingDiscountsData;
   }
 }
